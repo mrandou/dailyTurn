23d7c356b0cae7faf72cd2174c6d16b8da6b1b09
Randoudou <randou.maxime@tessan.io>
2024-09-16 17:44:44 +0200

    revert: :coffin: good bye my friends

--- src/app/models/players.ts
+++ src/app/models/players.ts
@@ -1,22 +1,19 @@
 export const Players: Player[] = [
-  { name: "Adrien", isAvailable: true, picture: "" },
-  { name: "Alexis", isAvailable: true, picture: "" },
-  { name: "Arnaud", isAvailable: true, picture: "" },
-  { name: "Aymen", isAvailable: true, picture: "" },
-  { name: "Bertrand", isAvailable: true, picture: "" },
-  { name: "Charles", isAvailable: true, picture: "" },
-  { name: "Elsa", isAvailable: true, picture: "" },
-  { name: "Gildas", isAvailable: true, picture: "" },
-  { name: "Julien", isAvailable: false, picture: "" },
-  { name: "Mathieu", isAvailable: true, picture: "" },
-  { name: "Maxime", isAvailable: true, picture: "" },
-  { name: "Mehdi", isAvailable: true, picture: "" },
-  { name: "Omar", isAvailable: true, picture: "" },
-  { name: "Rawan", isAvailable: false, picture: "" },
-  { name: "Sylvain", isAvailable: false, picture: "" },
-  { name: "Yi-Ching", isAvailable: true, picture: "" },
-  { name: 'Yichao', isAvailable: true, picture: "" }
-]
+  { name: 'Alexis', isAvailable: true, picture: '' },
+  { name: 'Arnaud', isAvailable: true, picture: '' },
+  { name: 'Aymen', isAvailable: true, picture: '' },
+  { name: 'Bertrand', isAvailable: true, picture: '' },
+  { name: 'Charles', isAvailable: true, picture: '' },
+  { name: 'Elsa', isAvailable: true, picture: '' },
+  { name: 'Gildas', isAvailable: true, picture: '' },
+  { name: 'Mathieu', isAvailable: true, picture: '' },
+  { name: 'Maxime', isAvailable: true, picture: '' },
+  { name: 'Mehdi', isAvailable: true, picture: '' },
+  { name: 'Omar', isAvailable: true, picture: '' },
+  { name: 'Rawan', isAvailable: false, picture: '' },
+  { name: 'Yi-Ching', isAvailable: true, picture: '' },
+  { name: 'Yichao', isAvailable: true, picture: '' },
+];
 
 export interface Player {
   name: string;
