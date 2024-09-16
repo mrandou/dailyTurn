0acadf1656db9e361e6dd0bf4faab85f3ceb652b
Randoudou <randou.maxime@tessan.io>
2024-09-16 17:45:20 +0200

    feat: :busts_in_silhouette: Add new challenger

--- src/app/models/players.ts
+++ src/app/models/players.ts
@@ -6,6 +6,7 @@ export const Players: Player[] = [
   { name: 'Charles', isAvailable: true, picture: '' },
   { name: 'Elsa', isAvailable: true, picture: '' },
   { name: 'Gildas', isAvailable: true, picture: '' },
+  { name: 'Khalifa', isAvailable: true, picture: '' },
   { name: 'Mathieu', isAvailable: true, picture: '' },
   { name: 'Maxime', isAvailable: true, picture: '' },
   { name: 'Mehdi', isAvailable: true, picture: '' },
